8dfe1cb5805e86a0819cccd89dd5ff38712b59e7
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-03-14 17:56:13 +0100

    imgCard

--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,14 @@
-import styled from "styled-components"
-
-const CardImage = styled.img`
-    border-radius: 25px;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-`
-
 function Card({ id, title, img }) {
     return (
-        <div className='conteneur'>{id}
-            <CardImage src={img} alt="{title}"/>
+        <div className='conteneur'>
+            <div className="imgcard">
+              <div src={img}></div>
                 <h5>{title}</h5>
+            </div>
         </div>
+
+
+        //onclick = {id}
     )
 }
 export default Card
