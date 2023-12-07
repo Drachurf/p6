30e0d18e41aed4409c13ae08e8c8c4e7f65150e9
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-12-07 12:36:19 +0100

    Updates

--- src/components/banner/banner.js
+++
@@ -1,9 +0,0 @@
-function Banner({ img, texte = false }) {
-    return (
-    <>
-      <img className="imagebanner" src={img} alt="payage de présentation"/>
-      {texte && <h1>{texte}</h1>}
-    </>
-  );
-}
-export default Banner;
