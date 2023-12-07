6081ecd5430de9740db2f952608f774a7c5622bb
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-12-07 11:39:12 +0100

    Uproute

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/footer.js";
 import Header from "./components/header/header.js";
 import Page404 from "../src/pages/page404/page404.js";
@@ -13,6 +13,7 @@ import "../src/css/app.css";
 function App() {
   return (
     <>
+    <Router basename="/p6">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -22,6 +23,7 @@ function App() {
         <Route path="/fiche/*" element={<Page404 />} />
       </Routes>
       <Footer />
+      </Router>
     </>
   );
 }
